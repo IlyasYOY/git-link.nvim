db7e3e381a1c0b33056c395d44ecfbb36a8bcb96
Ilia Ilinykh <ilyasyoy@gmail.com>
2023-05-25 18:34:49 +0300

    feat: http choice dialog
    implemented http choice so I'm able to work with insecure git repos.

--- lua/git-link/init.lua
+++ lua/git-link/init.lua
@@ -3,6 +3,21 @@ local utils = require "git-link.utils"
 
 local M = {}
 
+local function finalize_link(link)
+    local should_http = vim.fn.input {
+        prompt = "Resolved link: "
+            .. link
+            .. "\nShould we make it http? (y — yes, no otherwise)\n",
+        cancelreturn = "n",
+    }
+    if should_http == "y" then
+        link = string.gsub(link, "^https", "http")
+    end
+
+    vim.notify("\nLink copied: " .. link)
+    coredor.save_to_exchange_buffer(link)
+end
+
 ---function copies a link to your origin remote repo.
 function M.copy_repo_link()
     local link = utils.resolve_repo_url()
@@ -10,8 +25,7 @@ function M.copy_repo_link()
         return
     end
 
-    vim.notify("Resolved link: " .. link)
-    coredor.save_to_exchange_buffer(link)
+    finalize_link(link)
 end
 
 ---function copies a link to a file of your origin remote repo.
@@ -28,14 +42,13 @@ function M.copy_repo_link_to_file()
     end
 
     local current_file = coredor.current_working_file()
-    local file_link = utils.resolve_link_to_current_working_file(
+    local link = utils.resolve_link_to_current_working_file(
         repo_link,
         branch,
         current_file
     )
 
-    vim.notify("Resolved link: " .. file_link)
-    coredor.save_to_exchange_buffer(file_link)
+    finalize_link(link)
 end
 
 ---function copies a link to a line of your origin remote repo.
@@ -53,15 +66,14 @@ function M.copy_repo_link_to_line()
 
     local current_file = coredor.current_working_file()
     local current_line = vim.api.nvim_win_get_cursor(0)[1]
-    local line_link = utils.resolve_link_to_current_line(
+    local link = utils.resolve_link_to_current_line(
         repo_link,
         branch,
         current_file,
         current_line
     )
 
-    vim.notify("Resolved link: " .. line_link)
-    coredor.save_to_exchange_buffer(line_link)
+    finalize_link(link)
 end
 
 return M
